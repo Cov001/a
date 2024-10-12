6527484682596d2457b8067629a0305649448c25
doki <cov6155@gmail.com>
2024-10-12 23:24:41 +0700

    a

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const io = new Server(httpServer, {
   cors: {
     origin:
       process.env.NODE_ENV === "production"
-        ? "https://video-confe-server.vercel.app"
+        ? "https://b-ochre-two.vercel.app/"
         : "http://localhost:3000",
   },
 });
